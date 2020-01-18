dfbfd7b55290120149967bbcd962a8d9f83e7032
kazenetu <rakudafactory@gmail.com>
2020-01-18 22:44:12 +0900

    exampleを追加

---
+++ example/class/TestClass.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#ifndef TESTCLASS_H
+#define TESTCLASS_H
+
+#include "ISubscribe.h"
+
+class TestClass :public ISubscribe
+{
+public:
+	TestClass(std::string name)
+		: instanceName(name)
+	{
+	}
+
+	/*
+	  メッセージ受信
+	*/
+	void ReceiveMessage(std::string message)
+	{
+		// メッセージを表示
+		std::cout << "name:" << instanceName << " message:" << message << "\n";
+	}
+
+private:
+	std::string instanceName;
+
+};
+
+#endif //TESTCLASS_H
