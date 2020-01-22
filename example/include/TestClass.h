5a735e025446dd1a27ebb61e0f23b162acb78fb2
kazenetu <rakudafactory@gmail.com>
2020-01-22 21:49:59 +0900

    実装例のディレクトリ構造変更追加分

---
+++ example/include/TestClass.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#ifndef TESTCLASS_H
+#define TESTCLASS_H
+
+#include <string>
+#include "ISubscribe.h"
+
+class TestClass :public ISubscribe
+{
+public:
+	TestClass(std::string name)
+		: instanceName(name)
+	{
+	}
+
+	/*
+	  メッセージ受信
+	*/
+	void ReceiveMessage(std::string message)
+	{
+		// メッセージを表示
+		std::cout << "name:" << instanceName << " message:" << message << "\n";
+	}
+
+private:
+	std::string instanceName;
+
+};
+
+#endif //TESTCLASS_H
---
+++ example/src/main.cpp
@@ -0,0 +1,19 @@
+﻿#include <memory>
+#include "../include/TestClass.h"
+#include "PubSubContainer.h"
+
+int main()
+{
+	// TestClassインスタンスを生成
+	auto test1 = std::make_unique<TestClass>("TEST1");
+	auto test2 = std::make_unique<TestClass>("TEST2");
+
+	// 購読登録
+	PubSubContainer::Add(test1.get());
+	PubSubContainer::Add(test2.get());
+
+
+	// 発行
+	PubSubContainer::Publish("Publish");
+}
+
