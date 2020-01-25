655d9087d76ca1c0fd96718675515084943da76a
kazenetu <rakudafactory@gmail.com>
2020-01-25 22:24:14 +0900

    make対象の修正

--- example/make.sh
+++ example/make.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # build pubsub-container
-make clean --directory=pubsub-container
-make --directory=pubsub-container
+make clean --directory=../pubsub-container
+make --directory=../pubsub-container
 
 # build example
-make clean --directory=example
-make --directory=example
+make clean
+make
 
